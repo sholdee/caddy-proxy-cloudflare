c2b48ccc0ea44a1b89965643e1a4a33510d1174b
sholdee <102821812+sholdee@users.noreply.github.com>
2024-11-13 11:07:28 -0600

    fix healthcheck
    Signed-off-by: sholdee <102821812+sholdee@users.noreply.github.com>

--- healthcheck.go
+++ healthcheck.go
@@ -1,39 +1,27 @@
 package main
 
 import (
-    "net"
+    "net/http"
     "os"
-    "strings"
     "time"
 )
 
 func main() {
-    // Set up a connection to the Caddy admin API
-    conn, err := net.DialTimeout("tcp", "127.0.0.1:2019", 5*time.Second)
+    client := &http.Client{
+        Timeout: 5 * time.Second,
+    }
+
+    req, err := http.NewRequest("GET", "http://127.0.0.1:2019/config/", nil)
     if err != nil {
         os.Exit(1)
     }
-    defer conn.Close()
+    req.Host = "127.0.0.1:2019"
 
-    // Craft a minimal HTTP GET request
-    request := "GET /config HTTP/1.1\r\nHost: 127.0.0.1\r\nConnection: close\r\n\r\n"
-    _, err = conn.Write([]byte(request))
-    if err != nil {
+    // Perform the request
+    resp, err := client.Do(req)
+    if err != nil || resp.StatusCode != http.StatusOK {
         os.Exit(1)
     }
 
-    // Read the response
-    buf := make([]byte, 1024)
-    n, err := conn.Read(buf)
-    if err != nil {
-        os.Exit(1)
-    }
-
-    // Check if the response contains "200 OK"
-    response := string(buf[:n])
-    if strings.Contains(response, "200 OK") {
-        os.Exit(0)
-    }
-
-    os.Exit(1)
+    os.Exit(0)
 }
